89f6bcaf22602d5fdb74d77f434fa609ec84dba8
siddiq <siddiqahmed882@gmail.com>
2023-12-09 11:18:28 +0500

    Add tailwind integration and update package.json

---
+++ src/data/index.ts
@@ -0,0 +1,37 @@
+import type { Feature } from '../types';
+
+export const features: Feature[] = [
+  {
+    title: 'Rapid Express',
+    description:
+      'Personalized Rapid Journeys: With Rapid Express, every journey becomes a tailored experience. Say goodbye to circuitous routes and hello to efficiency.',
+  },
+  {
+    title: 'Shared Express',
+    description:
+      "Eco-Friendly Ride-Sharing: Shared Express is not just about sharing rides; it's a commitment to reducing our carbon footprint. Join the community that believes in sustainable commuting",
+  },
+  {
+    title: 'Fixed Express',
+    description:
+      "Reliable Daily Fixed Routes: Fixed Express is your dependable companion for daily routes. Whether it's your office, college, or any fixed destination, we've got your daily commute covered.",
+  },
+];
+
+export const keyBenefits: Feature[] = [
+  {
+    title: 'Innovation Hub',
+    description:
+      "Pirayo is not just a ride-sharing platform; it's an innovation hub. We constantly evolve to meet the ever-changing needs of our commuters.",
+  },
+  {
+    title: 'Community-Centric',
+    description:
+      "Join a community that values connection. Pirayo is not just about reaching your destination; it's about forging connections on the way.",
+  },
+  {
+    title: 'Efficiency Redefined',
+    description:
+      "With Pirayo, efficiency is not just a word; it's a promise. Experience a commute that is not only quick but also tailored to your preferences",
+  },
+];
